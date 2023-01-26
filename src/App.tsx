c61906de522352a18d253f3b8abb50447e667a81
Michał Obara <mobaradev@yahoo.com>
2023-01-25 23:32:39 -0800

    Content added to the main App component and background color changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Panel from "./components/Panel/Panel";
 import styled from "styled-components";
+import Stack from "./components/Stack/Stack";
+import Center from "./components/Base/Center/Center";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: auto;
-  background-color: antiquewhite;
+  background-color: aliceblue;
+`;
+
+const StackContainer = styled.div`
+  width: calc(100% - 240px);
+  height: 100%;
+  overflow: auto;
+  padding: 200px 0;
+  float: left;
 `;
 
 
@@ -14,6 +24,11 @@ function App() {
   return (
     <Container>
       <Panel />
+        <StackContainer>
+            <Center>
+                <Stack />
+            </Center>
+        </StackContainer>
     </Container>
   );
 }
