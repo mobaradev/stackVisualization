1e96a410c98653886d480fd504997cf63c71e1e0
Michał Obara <mobaradev@yahoo.com>
2023-01-25 23:30:49 -0800

    Number Input added as a component

---
+++ src/components/NumberInput/NumberInput.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const NumberInput = styled.input`
+  width: 96px;
+  height: 64px;
+  padding-top: 8px;
+  background: none;
+  border: none;
+  user-select: none;
+  font-size: 36px;
+  border-bottom: 3px solid silver;
+  text-align: center;
+  transition: 0.5s;
+  margin-bottom: 24px;
+  float: left;
+  
+  &:hover {
+    border-bottom: 3px solid gray;
+  }
+  
+  &:focus {
+    outline-style: dotted;
+  }
+`;
+
+export default NumberInput;
