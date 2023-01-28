bfa8858d6b10ee2bbe23f497a682be79513fd168
Michał Obara <mobaradev@yahoo.com>
2023-01-28 02:02:24 -0800

    Stack element (UI) transitions added

--- src/components/Stack/StackElement/StackElement.tsx
+++ src/components/Stack/StackElement/StackElement.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import {useEffect, useState} from "react";
 
-const Container = styled.div`
+const Container = styled.div<{visibilityLevel: 0 | 1, isTransition: boolean}>`
   width: 120px;
   height: 60px;
   background-color: aquamarine;
@@ -10,11 +11,42 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   border: 1px solid #41866e;
+
+  opacity: ${props => props.visibilityLevel};
+  transform: scale(${props => props.visibilityLevel});
+  transition: ${props => props.isTransition ? "transform 1s, opacity 1s" : "none"};
 `;
 
 function StackElement(props: any) {
+    const [visibilityLevel, setVisibilityLevel] = useState<0 | 1>(1);
+    const [isTransition, setIsTransition] = useState(true);
+
+    useEffect(() => {
+        if (props.index === 0) {
+            if (props.actionPending == "") {
+                setIsTransition(false);
+                setVisibilityLevel(1);
+            } else if (props.actionPending === "push") {
+                setIsTransition(false);
+                setVisibilityLevel(0);
+                setTimeout(() => {
+                    setIsTransition(true);
+                    setVisibilityLevel(1);
+                }, 100)
+
+            } else if (props.actionPending === "pop") {
+                setTimeout(() => {
+                    setIsTransition(true);
+                    setVisibilityLevel(0);
+                }, 50)
+
+            }
+        }
+    }, [props.index, props.actionPending]);
+
+
     return(
-        <Container>
+        <Container visibilityLevel={visibilityLevel} isTransition={isTransition}>
             {props.value}
         </Container>
     )
