bc6e2e060e9fdbadda0f1a899cdd4000949c840f
Michał Obara <mobaradev@yahoo.com>
2023-01-27 17:28:41 -0800

    React strict mode disabled and AppController init call added to React index file

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppController from "./logic/AppController/AppController";
+
+AppController.init();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
