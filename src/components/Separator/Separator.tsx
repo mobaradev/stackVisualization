b218c99f6fa18ec3f7184b3429ba30e248b31dd9
Michał Obara <mobaradev@yahoo.com>
2023-01-30 02:04:28 -0800

    Separator can be transparent

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
-const Separator = styled.div`
+const Separator = styled.div<{transparent?: boolean}>`
   margin: 18px 0;
   border: 0;
   border-top: 1px solid silver;
+  
+  ${props => props.transparent ? "opacity: 0;" : ""}
 `;
 
 export default Separator;
