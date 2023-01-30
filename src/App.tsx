30ecbccff02d4d8985e91c704c9ef1edba8c4ca2
Michał Obara <mobaradev@yahoo.com>
2023-01-30 02:11:19 -0800

    Views added to App and handling management of their visibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import Panel from "./components/Panel/Panel";
 import styled from "styled-components";
 import Stack from "./components/Stack/Stack";
 import Center from "./components/Base/Center/Center";
+import StartView from "./views/StartView/StartView";
+import AboutView from "./views/AboutView/AboutView";
 
 const Container = styled.div`
   width: 100vw;
@@ -19,18 +21,47 @@ const StackContainer = styled.div`
   float: left;
 `;
 
+type AppContextType = {
+    visibleScreens: number[],
+    setScreenVisibility: (id: number, visibility: boolean) => void
+};
+
+export const AppContext = createContext<AppContextType>({
+    visibleScreens: [],
+    setScreenVisibility: (id, visibility) => {}
+});
 
 function App() {
-  return (
+    const [visibleScreens, setVisibleScreens] = useState<number[]>([0]);
+
+    const setScreenVisibility = (id: number, visibility: boolean) => {
+        setVisibleScreens(visibleScreens => {
+            if (visibility) {
+                if (visibleScreens.indexOf(id) === -1) visibleScreens.push(id);
+            } else {
+                if (visibleScreens.indexOf(id) !== -1) visibleScreens.splice(visibleScreens.indexOf(id), 1);
+            }
+            return [...visibleScreens]
+        });
+    }
+
+    return (
     <Container>
-      <Panel />
-        <StackContainer>
-            <Center>
-                <Stack />
-            </Center>
-        </StackContainer>
+        <AppContext.Provider value={{visibleScreens: visibleScreens, setScreenVisibility: setScreenVisibility}}>
+            <StartView />
+            <AboutView />
+
+            <>
+                <Panel />
+                <StackContainer>
+                    <Center>
+                        <Stack />
+                    </Center>
+                </StackContainer>
+            </>
+        </AppContext.Provider>
     </Container>
-  );
+    );
 }
 
 export default App;
