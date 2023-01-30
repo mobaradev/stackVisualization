60d356eb95b038605325b31db6767d04a55b3c47
Michał Obara <mobaradev@yahoo.com>
2023-01-30 02:09:13 -0800

    Stack (UI) styled and added reload function binded to AppController

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -2,22 +2,51 @@ import StackElement from "./StackElement/StackElement";
 import styled from "styled-components";
 import {useEffect, useReducer, useState} from "react";
 import AppController from "../../logic/AppController/AppController";
+import Separator from "../Separator/Separator";
+import Center from "../Base/Center/Center";
+import Clearfix from "../Base/Clearfix/Clearfix";
 
-const Container = styled.div`
-  border: 2px solid gray;
+const Container = styled.div<{usePushAnimation?: boolean, usePopAnimation?: boolean}>`
+  width: 160px;
   border-radius: 12px;
+  background-color: white;
+  box-shadow: 0px 0 6px #cecbcb;
   padding: 20px;
+  transition: all 0.5s;
+  user-focus: none;
+  user-select: none;
+  touch-action: none;
+  ${props => props.usePushAnimation ? "transform: scale(1.03);" : ""}
+  ${props => props.usePopAnimation ? "transform: scale(0.97);" : ""}
 `;
 
+const InformationPanel = styled.div`
+  text-align: center;
+  
+  h1, p {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const EmptyStackInfo = styled.i`
+  font-size: 14px;
+`
+
 function Stack() {
-    const [elements, setElements] = useState<number[]>([1,2,3]);
+    const [elements, setElements] = useState<number[]>([]);
     const [numberOfOperations, setNumberOfOperations] = useState(0);
     const [actionPending, setActionPending] = useState("");
 
     useEffect(() => {
         AppController.onPush = push;
         AppController.onPop = pop;
+        AppController.onStackReload = reload;
     }, []);
+
+    const reload = () => {
+        setElements([...AppController.stack.elements]);
+    }
 
     const push = (value: number) => {
         setElements( prevElements => [value, ...prevElements]);
@@ -49,11 +78,27 @@ function Stack() {
     }
 
     return(
-        <Container onClick={pop}>
-            Action: {actionPending}
+        <Container usePushAnimation={actionPending === "push"} usePopAnimation={actionPending === "pop"}>
+            <InformationPanel>
+                <h1>Stack</h1>
+            </InformationPanel>
+            <Separator />
             {
                 elements.map((element, index) =>
                     <StackElement index={index} actionPending={actionPending} key={index + "-" + numberOfOperations} value={element} />)
+            }
+
+            {
+                elements.length === 0
+                ?
+                    <>
+                        <Center>
+                        <EmptyStackInfo>The stack is empty</EmptyStackInfo>
+                        </Center>
+                        <Clearfix />
+                    </>
+                :
+                    ""
             }
         </Container>
     )
