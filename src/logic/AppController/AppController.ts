6633141f75199fda76bc030f26935d350e408854
Michał Obara <mobaradev@yahoo.com>
2023-01-30 18:11:32 -0800

    AppController version, onTop() and onStackUpdate() added

--- src/logic/AppController/AppController.ts
+++ src/logic/AppController/AppController.ts
@@ -4,9 +4,14 @@ class AppController {
     static stack: StackStructure;
     static onPush: (value: number) => void;
     static onPop: () => void;
+    static onTop: () => void;
+    static onStackUpdate: () => void;
     static onStackReload: () => void;
 
+    static version: string;
+
     static init() {
+        AppController.version = "1.00"
         AppController.stack = new StackStructure();
     }
 }
